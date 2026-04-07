e2310a63ea2183c7e9b624358cc232f1b92853d5
JuanPabloPadillCarvajal <157859295+JuanPabloPadillCarvajal@users.noreply.github.com>
2026-04-07 22:08:59 +0000

    tutorial 6 hecho

---
+++ backend/src/home/home.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+
+
+@Controller()
+
+export class HomeController {
+
+@Get()
+
+index(): string {
+
+return 'API is running';
+
+}
+
+}
---
+++ backend/src/home/home.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+
+import { HomeController } from './home.controller';
+
+
+@Module({
+
+controllers: [HomeController],
+
+})
+
+export class HomeModule {}
